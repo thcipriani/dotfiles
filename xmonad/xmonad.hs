ac2bb26d5cc47046f4e137f4532cb6c81ed56758
Tyler Cipriani <tyler@tylercipriani.com>
2013-12-14 18:08:46 -0700

    XMONAD fool

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,32 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+
+-- Used for isFullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat) 
+
+-- Used for xmobar
+import XMonad.Util.Run(spawnPipe)
+
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    , isFullscreen --> doFullFloat
+    ]
+
+main = do
+    xmproc <- spawnPipe "xmobar /home/tyler/.xmobarrc"
+    xmonad $ defaultConfig { 
+        terminal = "uxterm"
+        , manageHook = manageDocks <+> myManageHook 
+                        <+> manageHook defaultConfig
+        , layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP { 
+            ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        }
