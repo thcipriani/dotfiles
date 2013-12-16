56e279f15a86f103707967daee98c2db62039a4d
Tyler Cipriani <tyler@tylercipriani.com>
2013-12-16 06:52:35 -0700

    Final xmonad tweaks

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,3 +1,6 @@
+--
+-- Imports
+-- =======================================
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -12,21 +15,33 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+--
+-- Window Rules
+-- =======================================
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Vncviewer" --> doFloat
     , isFullscreen --> doFullFloat
     ]
 
+--
+-- Main Layout
+-- =======================================
 main = do
     xmproc <- spawnPipe "xmobar /home/tyler/.xmobarrc"
-    xmonad $ defaultConfig { 
+    xmonad $ defaultConfig
+      { 
         terminal = "urxvt"
-        , manageHook = manageDocks <+> myManageHook 
+        , manageHook = manageDocks 
+                        <+> myManageHook 
                         <+> manageHook defaultConfig
         , layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP { 
+        , workspaces = ["1:web","2:term"] ++ map show [3..9]
+
+        -- Gets piped to xmobar
+        , logHook = dynamicLogWithPP xmobarPP
+          { 
             ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
-        }
+            , ppTitle = xmobarColor "#859900" "" . shorten 50
+          }
+     }
