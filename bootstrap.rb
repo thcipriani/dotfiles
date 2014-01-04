80528b3bc8dff71e100962eda31a33d6233f3c0e
Tyler Cipriani <tyler@tylercipriani.com>
2014-01-04 12:33:49 -0700

    Add scripts, move ~/.bin to ~/bin

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
-
 # from http://errtheblog.com/posts/89-huba-huba
 
 home = File.expand_path('~')
+dontlink = %w[bin bootstrap]
 
 Dir['*'].each do |file|
-  next if file =~ /install/
+  next if file =~ /#{dontlink.join('|')}/
   target = File.join(home, ".#{file}")
   `ln -sf #{File.expand_path file} #{target}`
 end
+
+# link bin to ~/bin
+`ln -sf #{File.expand_path 'bin'} #{File.join(home, 'bin')}`
 
 # git push on commit
 `echo 'git push' > .git/hooks/post-commit`
