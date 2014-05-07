52f6fa378f022a707c71557f5e80b607fd78b085
Tyler Cipriani <tyler@tylercipriani.com>
2014-05-07 17:16:09 -0600

    xbindkysrc to replace middle click

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,24 +1,40 @@
 #!/usr/bin/env ruby
 # from http://errtheblog.com/posts/89-huba-huba
 
-home = File.expand_path('~')
-dontlink = %w[bin srv bootstrap]
+home = File.expand_path '~'
+dotfiles = File.expand_path '~/.dotfiles'
+
+dontlink = %w[bin srv bootstrap project plan]
 otherbin = %w[sshit weather-ma-jig]
 
 Dir['*'].each do |file|
   next if file =~ /#{dontlink.join('|')}/
   target = File.join(home, ".#{file}")
-  `ln -sf #{File.expand_path file} #{target}`
+  `ln -sf "#{File.join dotfiles, file}" "#{target}"`
 end
 
-# link bin to ~/bin
-`ln -sf #{File.expand_path 'bin'} #{File.join(home, 'bin')}`
-`ln -sf #{File.expand_path 'srv'} #{File.join(home, 'srv')}`
+# link bin and srv
+%w[bin srv].each do |dir|
+  `ln -sf "#{File.join dotfiles, dir}" "#{File.join(home, dir)}"`
+end
+
+# Hardlinks to satisfy finger
+%w[project plan].each do |file|
+  target = File.join(home, ".#{file}")
+  `ln "#{File.join dotfiles, file}" "#{target}"`
+end
 
 # link other repo executables so I don't have to deal with submodules
 otherbin.each do |repo|
   `git clone git@github.com:thcipriani/#{repo}.git #{File.expand_path 'bin'}/src/#{repo}`
 end
+
+# Vundle setup
+if not File.directory? File.expand_path '~/.vim/bundle/Vundle.vim'
+  `git clone https://github.com/gmarik/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"`
+end
+
+`vim +PluginInstall +qall`
 
 # git push on commit
 `echo 'git push' > .git/hooks/post-commit`
