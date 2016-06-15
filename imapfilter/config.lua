678ac7b180423610b608dd5fcad709142bc6e3a8
Tyler Cipriani <tcipriani@wikimedia.org>
2016-06-15 11:36:59 -0700

    mail things

--- imapfilter/config.lua
+++ imapfilter/config.lua
@@ -23,19 +23,37 @@ account = IMAP {
 
 inbox = account['INBOX']
 
-account:create_mailbox('ML')
-account:create_mailbox('ML/wmfall')
+-- account:create_mailbox('ML')
+-- account:create_mailbox('ML/wmfall')
 results = inbox:contain_to('wmfall@lists.wikimedia.org')
 results:move_messages(account['ML/wmfall'])
 
-account:create_mailbox('ML/wikitech-l')
+-- account:create_mailbox('ML/wikitech-l')
 results = inbox:contain_to('wikitech-l@lists.wikimedia.org')
 results:move_messages(account['ML/wikitech-l'])
 
-account:create_mailbox('CR')
+results = inbox:contain_to('ops@lists.wikimedia.org')
+results:move_messages(account['ML/ops'])
+
+-- account:create_mailbox('ML/debian-devel')
+results = inbox:contain_to('debian-devel@lists.debian.org')
+results:move_messages(account['ML/debian-devel'])
+
+-- account:create_mailbox('CR')
+
+-- Gerrit
 results = inbox:contain_from('gerrit@wikimedia.org')
 results:move_messages(account['CR'])
 
-account:create_mailbox('CI')
+-- Phabricator-mail-tags that contain differential
+-- X-Phabricator-Mail-Tags: <differential-review-request>, <differential-other>, <differential-reviewers>
+-- results = inbox:contain_field('X-Phabricator-Mail-Tags', '<differential-review-request>')
+-- results = inbox:match_header('.*X-Phabricator-Mail-Tags: <differential-review-request>.*')
+results = inbox:contain_subject('[Differential]')
+results:move_messages(account['CR'])
+
+-- account:create_mailbox('auto')
 results = inbox:contain_from('jenkins-bot@wikimedia.org')
-results:move_messages(account['CI'])
+results:move_messages(account['auto'])
+results = inbox:contain_to('betacluster-alerts@lists.wikimedia.org')
+results:move_messages(account['auto'])
