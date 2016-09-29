a83df43cd1236fcfbe6b57ab5578485d6bec39cd
Tyler Cipriani <tcipriani@wikimedia.org>
2016-09-28 19:37:37 -0700

    git annex fix

--- srv/photos/lib/convert-nef.sh
+++ srv/photos/lib/convert-nef.sh
@@ -1,1 +1,1 @@
-../.git/annex/objects/pp/w6/SHA256E-s455--e8b43c6137f7c84bdce6c9e986b4c2cfeb75708b3d293d6fa7f027ad368e2431.sh/SHA256E-s455--e8b43c6137f7c84bdce6c9e986b4c2cfeb75708b3d293d6fa7f027ad368e2431.sh
+../../../.git/annex/objects/pp/w6/SHA256E-s455--e8b43c6137f7c84bdce6c9e986b4c2cfeb75708b3d293d6fa7f027ad368e2431.sh/SHA256E-s455--e8b43c6137f7c84bdce6c9e986b4c2cfeb75708b3d293d6fa7f027ad368e2431.sh
